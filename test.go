ed30b474bf56eb654baba49c108e6ff2978f7d4e
DIGIFEMMES-22LAB024 <ndiaissatou@gmail.com>
2024-05-17 12:30:32 +0000

    aissat

--- test.go
+++ test.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("coucou les filles")
+	fmt.Println("coucou les filles , ou allez vous? au marché?")
 }
 func euro() {
-	fmt.Println("comment vous allez, bien j'espere!")
+	fmt.Println("comment vous allez, bien j'espere !")
 }
