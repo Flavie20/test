920454bbb91d3607aeeebacbe7a4bf4e7acb84f9
flavie20 <flavieama4@gmail.com>
2024-05-17 11:12:10 +0000

    euro

--- test.go
+++ test.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("coucou les filles")
+	fmt.Println("coucou les filles , ou allez vous? au marché?")
 }
 func euro() {
-	fmt.Println("comment vous allez ?")
+	fmt.Println("comment vous allez, bien j'espere")
 }
